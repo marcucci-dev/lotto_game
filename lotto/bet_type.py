677c4576be157dd70c7d5159225a03aaea2236cc
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-11-30 01:31:34 +0100

    added bet_type_allowed

--- lotto/bet_type.py
+++ lotto/bet_type.py
@@ -1,19 +1,35 @@
 class BetType:
-    bet_types = {'Ambata': 1, 'Ambo': 2, 'Terno': 3, 'Quaterna': 4, 'Cinquina': 5}
+    """Represents a type of bet of a ticket.
+
+    Attributes:
+        __bet_type: int     is a index for bet_type_allowed list
+    """
+    # bet_types = {'Ambata': 1, 'Ambo': 2, 'Terno': 3, 'Quaterna': 4, 'Cinquina': 5}
+    bet_type_allowed = ['Ambata', 'Ambo', 'Terno', 'Quaterna', 'Cinquina']
 
     def __init__(self, bet_type):
-        self.bet_type = bet_type  # bet_type:
+        self.__bet_type = bet_type  # bet_type:
+
+    def get(self):
+        return self.__bet_type
 
     def get_name(self):
-        for i in BetType.bet_types:
-            if BetType.bet_types[i] == self.bet_type:
-                return i
+        return BetType.bet_type_allowed[self.__bet_type]
+
+    @staticmethod
+    def get_max_index():
+        return len(BetType.bet_type_allowed) - 1
+
+    @staticmethod
+    def get_min_index():
+        return 0
 
 
 def test():
-    bet_type_01 = BetType(1)
-    name = bet_type_01.get_name()
-    print(name)
+    for i in range(len(BetType.bet_type_allowed)):
+        bet_type_01 = BetType(i)
+        name = bet_type_01.get_name()
+        print(name)
 
 
 if __name__ == '__main__':
