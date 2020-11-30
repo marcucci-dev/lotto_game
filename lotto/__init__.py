ae4780ede5066b7c48552a32906a51f7892d93ed
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-11-30 02:17:27 +0100

    add __init__.py file

---
+++ lotto/__init__.py
@@ -0,0 +1,12 @@
+import os
+import sys
+#del sys.path[0]
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) )
+#print("A", sys.path)
+"""__all__ = [
+    'bet_type',
+    'table',
+    'ticket',
+    'city'
+    ]
+"""
