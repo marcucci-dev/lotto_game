62385410f2ff4787f47bc9c5145fabdbb42e5b82
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-12-06 19:53:46 +0100

    add min_amount_numbers attribute

--- lotto/bet_type.py
+++ lotto/bet_type.py
@@ -7,8 +7,9 @@ class BetType:
     # bet_types = {'Ambata': 1, 'Ambo': 2, 'Terno': 3, 'Quaterna': 4, 'Cinquina': 5}
     bet_type_allowed = ['Ambata', 'Ambo', 'Terno', 'Quaterna', 'Cinquina']
 
-    def __init__(self, bet_type):
+    def __init__(self, bet_type=0):
         self.__bet_type = bet_type  # bet_type:
+        self.min_amount_numbers = bet_type + 1
 
     def get(self):
         return self.__bet_type
