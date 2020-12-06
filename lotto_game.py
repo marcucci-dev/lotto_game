079f8fe4877b8906fa32e838922569d754b4ff62
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-12-06 20:02:12 +0100

    add check_winning_ticket

--- lotto_game.py
+++ lotto_game.py
@@ -18,12 +18,24 @@ def main():
         ticket_list = []
         for i in range(num_of_tickets):
             print("-----\nTicket number", i + 1)
-            LottoController.insert_ticket(ticket_list)
+            LottoController.insert_ticket(ticket_list, i)
 
         # for ticket in ticket_list:
         for i in range(len(ticket_list)):
             LottoView.show_ticket(ticket_list[i], i+1)
             print()
+
+        # make an extraction
+        extraction = LottoController.create_extraction()
+        # LottoView.show_extraction(extraction)
+
+        # for ticket in ticket_list check combinations wins
+        for i in range(len(ticket_list)):
+            winning = LottoView.check_winning_ticket(ticket_list[i], extraction)
+            if winning:
+                assert isinstance(winning, object)
+                winners.append(winning)
+                # LottoView.show_winners(winning)
 
         print('\n'+'_'*80)
         main()
