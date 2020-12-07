866c44e2835e9eb91713081ce08f9445e0b8ffbf
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-12-07 11:29:41 +0100

    add all_cities attribute

--- lotto/lotto_controller.py
+++ lotto/lotto_controller.py
@@ -3,6 +3,8 @@ from bet_type import BetType
 from ticket import Ticket
 from extraction import Extraction
 from winning_combination import WinningCombination
+from lotto_view import LottoView
+
 
 class LottoController:
     @staticmethod
@@ -47,27 +49,30 @@ class LottoController:
 
     @staticmethod
     def select_city():
-        suffix = ""
-        for j in range(City.get_max_index()+1):
-            suffix += "\n\t" + str(j) + ": " + City.city_allowed[j] + " "
+        suffix = "\n\t0: Tutte "
+        for j in range(City.get_max_index() + 1):
+            suffix += "\n\t" + str(j + 1) + ": " + City.city_allowed[j] + " "
         prompt = "Choose the \"city\" (aka \"ruota\"): " + suffix + "\n"
-        city_index = LottoController.check_input(prompt, min_value=City.get_min_index(), max_value=City.get_max_index())
-        a_city = City(city_index)
+        all_cities_value = 0
+        city_index = LottoController.check_input(prompt, min_value=all_cities_value, max_value=City.get_max_index() + 1)
+        all_cities = city_index == 0
+        a_city = City(all_cities=all_cities, city=city_index - 1)
         return a_city
 
     @staticmethod
     def select_bet_type():
         suffix = ""
-        for j in range(BetType.get_max_index()+1):
-            suffix += "\n\t" + str(j+1) + ": " + str(BetType.bet_type_allowed[j]) + " "
+        for j in range(BetType.get_max_index() + 1):
+            suffix += "\n\t" + str(j + 1) + ": " + str(BetType.bet_type_allowed[j]) + " "
         prompt = "Choose the type of bet:" + suffix + "\n"
-        bet_type = LottoController.check_input(prompt, min_value=BetType.get_min_index() + 1, max_value=BetType.get_max_index() + 1)
-        a_bet_type = BetType(bet_type-1)
+        bet_type = LottoController.check_input(prompt, min_value=BetType.get_min_index() + 1,
+                                               max_value=BetType.get_max_index() + 1)
+        a_bet_type = BetType(bet_type - 1)
         return a_bet_type
 
     @staticmethod
     def select_amount_of_numbers(bet_type):
-        min_for_bet_type = bet_type.get()+1
+        min_for_bet_type = bet_type.get() + 1
         max_for_bet_type = 10
         prompt = "Choose the amount of numbers to generate (min {} max {} per this bet) \n".format(min_for_bet_type,
                                                                                                    max_for_bet_type)
@@ -88,15 +93,15 @@ class LottoController:
     @staticmethod
     def check_winning_ticket(ticket, extraction):
         winning_combinations = list()
-        # for city in ticket.city:
-        if True:
-            city = ticket.city
+        for city in ticket.city.get_name():
+            # city = ticket.city
             winning_numbers = list()
             for number in ticket.numbers:
-                if number in extraction.drawn_numbers[city.get_name()]:
+                if number in extraction.drawn_numbers[city]:
                     winning_numbers.append(number)
             if len(winning_numbers) >= ticket.bet_type.min_amount_numbers:
-                winning_combination = WinningCombination(city, ticket.bet_type, winning_numbers, ticket.id)
+                winning_combination = WinningCombination(city, ticket.bet_type, winning_numbers, len(ticket.numbers),
+                                                         ticket.id)
                 winning_combinations.append(winning_combination)
         return winning_combinations
 
@@ -105,24 +110,19 @@ if __name__ == '__main__':
     # bet_type_01 = LottoController.select_bet_type()
     # print(bet_type_01.get_name())
 
-    city_01 = City(1)  # manca il caso TUTTE
+    city_01 = City(all_cities=True)
     bet_type_01 = BetType(0)
     amount_of_numbers_01 = 10  # amount_of_numbers
-    t = Ticket(city_01, bet_type_01, amount_of_numbers_01)
-    
-    ex = LottoController.create_extraction()
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    t = Ticket(city_01, bet_type_01, amount_of_numbers_01, numbers)
+
+    # ex = LottoController.create_extraction()
+    drawn_numbers = {'Bari': [1, 2, 3, 4, 5], 'Cagliari': [6, 7, 8, 9, 10], 'Firenze': [1, 3, 5, 7, 9],
+                     'Genova': [13, 18, 11, 79, 17], 'Milano': [77, 48, 72, 84, 1], 'Napoli': [21, 81, 73, 87, 69],
+                     'Palermo': [35, 36, 21, 12, 22], 'Roma': [11, 30, 7, 38, 24], 'Torino': [50, 4, 38, 9, 84],
+                     'Venezia': [2, 4, 6, 8, 10]}
+    ex = Extraction(drawn_numbers_=drawn_numbers)
 
     w_c = LottoController.check_winning_ticket(t, ex)
     for w in w_c:
-        print(w)
-
-        print(1, w.city, w.bet_type, w.winning_numbers)
-        print(w.city.get_name())
-        print(w.bet_type.get_name())
-        print(w.winning_numbers)
-        print(w.amount_winning_combinations)
-
-
-
-    
-    
+        LottoView.show_winners(w)
