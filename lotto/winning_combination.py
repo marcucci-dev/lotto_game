f7c4442e8dac70d88cb36641ecdc07e2cd2accc2
Giuseppe Marcucci <marcucci.dev@gmail.com>
2020-12-14 18:10:01 +0100

    add class Winner & add prize to WinningCombination

--- lotto/winning_combination.py
+++ lotto/winning_combination.py
@@ -1,5 +1,16 @@
 from itertools import combinations
 
+prize_for_one = {1: [11.23],
+                 2: [ 5.62, 250.00],
+                 3: [ 3.74,  83.33, 4500.00],
+                 4: [ 2.81,  41.67, 1125.00, 120000.00],
+                 5: [ 2.25,  25.00,  450.00,  24000.00, 6000000.00],
+                 6: [ 1.87,  16.67,  225.00,   8000.00, 1000000.00],
+                 7: [ 1.60,  11.90,  128.57,   3428.57,  285714.29],
+                 8: [ 1.40,   8.93,   80.36,   1714.29,  107142.86],
+                 9: [ 1.25,   6.94,   53.57,    952.38,   47619.05],
+                 10:[ 1.12,   5.56,   37.50,    571.43,   23809.52]}
+
 
 class WinningCombination:
     def __init__(self, city, bet_type, winning_numbers, amount_numbers_played, ticket_id):
@@ -17,3 +28,18 @@ class WinningCombination:
         self.winning_numbers = winning_numbers
         self.amount_winning_combinations = len(list(combinations(winning_numbers, bet_type.min_amount_numbers)))
         self.amount_numbers_played = amount_numbers_played
+
+        self.prize = prize_for_one[self.amount_numbers_played][self.bet_type.get()] * self.amount_winning_combinations
+
+
+class Winner:
+    def __init__(self, winning_combinations, total_prize, ticket_id):
+        """
+
+        Args:
+            ticket_id
+            winning_combinations List[WinningCombination]:
+        """
+        self.ticket_id = ticket_id
+        self.total_prize = total_prize
+        self.winning_combinations = winning_combinations
